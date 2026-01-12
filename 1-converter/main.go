9d54b13fb1d9251a50019cdcfe7e4dc86d2f1b8e
Amal Dusalin <dekart416@yandex-team.ru>
2026-01-12 17:29:54 +0500

    strings and functions

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -9,11 +9,31 @@ const EUR_2_RUB = USD_2_RUB / USD_2_EUR
 func main() {
 	fmt.Println("Введите сумму в EUR:")
 
-	var eur_sum float64
+	eur_sum, err := getSum()
 
-	fmt.Scan(&eur_sum)
+	if err != nil {
+		fmt.Println("Ошибка ввода")
+
+		return
+	}
+
+	calculate(eur_sum, "EUR", "RUB")
 
 	rub_sum := eur_sum * EUR_2_RUB
 
 	fmt.Printf("Сумма в RUB: %.2f\n", rub_sum)
+}
+
+func calculate(value float64, src_currency string, dst_currency string) {}
+
+func getSum() (float64, error) {
+	var eur_sum float64
+
+	_, err := fmt.Scan(&eur_sum)
+
+	if err != nil {
+		return eur_sum, err
+	}
+
+	return eur_sum, nil
 }
