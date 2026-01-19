e3b3c276cdbe906dab6ef13773e6ea839439343b
Amal Dusalin <dekart416@yandex-team.ru>
2026-01-19 15:24:16 +0300

    os commit

--- 3-struct/bins/bins.go
+++ 3-struct/bins/bins.go
@@ -1,1 +1,25 @@
 package bins
+
+import "time"
+
+type Bin struct {
+	Id        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+}
+
+type BinList []Bin
+
+func NewBin(id string, private bool, name string) Bin {
+	return Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+}
+
+func NewBinList() BinList {
+	return BinList{}
+}
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,1 +1,34 @@
 package storage
+
+import (
+	"3-struct/bins"
+	"3-struct/file"
+	"encoding/json"
+	"os"
+)
+
+const STORAGE_FILE = "storage.json"
+
+func Save(data bins.BinList) error {
+	jsonData, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(STORAGE_FILE, jsonData, 0644)
+}
+
+func Get() (bins.BinList, error) {
+	jsonData, err := file.ReadFile(STORAGE_FILE)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var data bins.BinList
+
+	err = json.Unmarshal(jsonData, &data)
+
+	return data, err
+}
