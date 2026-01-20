780d2b752fce8468a58b1f15eb2d3eced55da21f
Amal Dusalin <dekart416@yandex-team.ru>
2026-01-20 19:04:13 +0300

    fix

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"3-struct/bins"
+	"3-struct/storage"
+	"fmt"
+)
+
+func main() {
+	bin := bins.NewBin("id123", true, "Vasya")
+	binList := bins.NewBinList()
+
+	binList = append(binList, bin)
+
+	storage := storage.JsonStorage{}
+
+	storage.Save(binList)
+
+	result, _ := storage.Get()
+
+	fmt.Println(result)
+}
